bbab2ce316354b990e91bd631d84dd37a672c64d
Aritra Mondal <aritra041999.dev@gmail.com>
2024-10-25 17:07:56 +0530

    feat: db connection setp

--- main.go
+++ main.go
@@ -1,11 +1,23 @@
 package main
 
 import (
+	"os"
+
+	"github.com/joho/godotenv"
 	"github.com/rs/zerolog/log"
 )
 
 func init() {
 	log.Print("Reading environment variables")
+
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Error().Err(err).Msg("error loading .env file")
+
+	}
+
+	dbConnString := os.Getenv("SECRET_XATA_PG_ENDPOINT")
+	log.Print(dbConnString)
 }
 
 func main() {
