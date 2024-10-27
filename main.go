641930ab01450e54cc49bee6e7388d5d74dbf754
Aritra Mondal <aritra041999.dev@gmail.com>
2024-10-28 00:11:32 +0530

    feat: load env vatiables and connect to the database

--- main.go
+++ main.go
@@ -1,14 +1,45 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
 	"os"
+	"time"
 
+	"github.com/google/uuid"
 	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 	"github.com/rs/zerolog/log"
 )
 
+type UptimeCheck struct {
+	ID        uuid.UUID
+	WebSiteId uuid.UUID
+	CreatedAt time.Time
+}
+
+func loadEnv() error {
+	log.Print("Loading environment variables")
+	if err := godotenv.Load(".env"); err != nil {
+		return err
+	}
+	return nil
+}
+
+func connectDB() (*sql.DB, error) {
+	log.Print("Connecting to database")
+	db, err := sql.Open("postgres", os.Getenv("SECRET_XATA_PG_ENDPOINT"))
+	if err != nil {
+		return nil, err
+	}
+	return db, nil
+}
+
 func init() {
-	log.Print("Reading environment variables")
+	if err := loadEnv(); err != nil {
+		log.Fatal().Err(err).Msg("error loading environment variables")
+	}
+}
 
 	err := godotenv.Load(".env")
 	if err != nil {
@@ -21,5 +52,6 @@ func init() {
 }
 
 func main() {
+	db, err := connectDB()
 
 }
