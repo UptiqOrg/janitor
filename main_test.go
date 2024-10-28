b6096858ef3124d1a83c5f7b32715dde4b03418f
Aritra Mondal <aritra041999.dev@gmail.com>
2024-10-28 12:05:46 +0530

    feat: add placeholder script to test test-setup

---
+++ main_test.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"janitor/db"
+	"janitor/tests"
+	"testing"
+
+	. "github.com/franela/goblin"
+)
+
+func TestMain(t *testing.T) {
+	g := Goblin(t)
+
+	postgresC, err := tests.Setup(t)
+	if err != nil {
+		t.Fatalf("failed to setup postgres container: %s", err)
+		return
+	}
+
+	dbConnString, err := tests.GetConnString(t, postgresC)
+	db.ConnectDB(dbConnString)
+
+	g.Describe("getExpiredUptimeChecks", func() {
+		g.It("should return a list of expired uptime checks", func() {
+			g.Assert(1).Equal(1)
+		})
+	})
+
+	tests.Teardown(&postgresC)
+}
